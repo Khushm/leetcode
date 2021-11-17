3f8225121ec50a9d4180ff4684339e2a249f55e0
Khushboo Mundada <mundadakhush2001@gmail.com>
2021-11-18 02:55:29 +0530

    Create main.cpp
     Best Time to Buy and Sell Stock

---
+++ 121/main.cpp
@@ -0,0 +1,18 @@
+//  Best Time to Buy and Sell Stock
+// Time Limit exceeded
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxDiff, maxElement, max = 0;
+        for(int i=0;i<prices.size();i++){
+            maxElement = *max_element(prices.begin()+i, prices.end());
+            maxDiff = maxElement - prices[i];
+            if(max < maxDiff){
+                max = maxDiff;
+            }
+
+        }
+        return max;
+    }
+};
