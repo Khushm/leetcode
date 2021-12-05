e69f8df1dc052593edc918abd057aafab29527a1
Khushboo Mundada <mundadakhush2001@gmail.com>
2021-12-06 00:45:42 +0530

    Invert Binary Tree

---
+++ 226/main2.cpp
@@ -0,0 +1,19 @@
+// Invert Binary tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int temp;
+    TreeNode* invertTree(TreeNode* root) {
+        return root ? (swap(root->left, root->right), invertTree(root->left), invertTree(root->right), root): root;
+    }
